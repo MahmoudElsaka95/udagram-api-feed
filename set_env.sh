3bf0f568a94a49a11fea4ef6c211dfec3c0d8e95
Mahmoud Elsaka <Mahmoud.elskaa20@gmail.com>
2021-03-13 22:24:49 +0200

    Initialize Repo

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=cdr01012945547
+export POSTGRES_HOST=microservices-dev.cxizaevyrwc6.eu-west-3.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udacity-bucket-dev 
+export AWS_REGION=eu-west-3
+export AWS_PROFILE=default
+export JWT_SECRET=
+export URL=http://localhost:8100
